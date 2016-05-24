ef7268eb7026980e6414721c6f0b0bc7ef687b81
Joshua Book <joshua.book@gmail.com>
2016-05-24 18:01:36 -0400

    four box bubble split out per side

--- app/index.js
+++ app/index.js
@@ -10,38 +10,104 @@ export default class App extends React.Component {
     super();
     this.state = {
       open: false,
+      hover: false,
     };
     this.toggle = this.toggle.bind(this);
+    this.mouseEnter = this.mouseEnter.bind(this);
+    this.mouseLeave = this.mouseLeave.bind(this);
   }
 
   toggle() {
     this.setState({open: !this.state.open});
   }
 
+  mouseEnter() {
+    this.setState({hover: !this.state.hover});
+  }
+
+  mouseLeave() {
+    this.mouseEnter();
+  }
+
+  boxStyles(boxSpring) {
+    const {open, hover} = boxSpring;
+    let boxSize = `${open}px`;
+
+    if (!this.state.open) {
+      boxSize = `${open * hover}px`;
+    }
+
+    return {
+      width: boxSize,
+      height: boxSize,
+    }
+  }
+
   render () {
     return (
       <div id='app'>
         <Motion
-          style={{x: spring(
-            this.state.open ? 400 : 0,
-            {stiffness: 160, damping: 13}
-          )}}
+          style={{
+            open: spring(
+              this.state.open ? 200 : 300,
+              {stiffness: 160, damping: 14}),
+            hover: spring(
+              this.state.hover ? 1.2 : 1,
+              {stiffness: 160, damping: 13}),
+          }}
         >
-          {(spring) => {
-            console.log(spring.x);
-              return (
-                <div
-                  id='box'
-                  style={{
-                    left: `${spring.x}px`,
-                    top: `${spring.x/4}px`
-                  }}
-                  onClick={this.toggle}
-                >
-                  {spring.x}
-                </div>
-              )
-            }
+          {(boxSpring) =>
+            <div
+              id='box'
+              onClick={this.toggle}
+              onMouseEnter={this.mouseEnter}
+              onMouseLeave={this.mouseLeave}
+              style={this.boxStyles(boxSpring)}
+            >
+              <Motion
+                style={{x: spring(
+                  this.state.open ? 300 : 50,
+                  {stiffness: 60, damping: 13}
+                )}}
+              >
+                {(minis) =>
+                  <div>
+                    <div
+                      id='mini-1'
+                      className='mini'
+                      style={{
+                        left: `${minis.x}px`,
+                        top: `${minis.x/6}px`
+                      }}
+                    ></div>
+                    <div
+                      id='mini-2'
+                      className='mini'
+                      style={{
+                        left: `${minis.x/6}px`,
+                        top: `${minis.x}px`
+                      }}
+                    ></div>
+                    <div
+                      id='mini-3'
+                      className='mini'
+                      style={{
+                        left: `${minis.x/6}px`,
+                        bottom: `${minis.x}px`
+                      }}
+                    ></div>
+                    <div
+                      id='mini-4'
+                      className='mini'
+                      style={{
+                        right: `${minis.x}px`,
+                        top: `${minis.x/6}px`
+                      }}
+                    ></div>
+                  </div>
+                }
+              </Motion>
+            </div>
           }
         </Motion>
       </div>
