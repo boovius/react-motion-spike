5aaabee06715f16daf904a3ec7bb84fe5c66335f
Joshua Book <joshua.book@gmail.com>
2016-05-24 11:10:26 -0400

    red moving ball on spring

--- app/index.js
+++ app/index.js
@@ -3,12 +3,47 @@ import React from 'react';
 import './stylesheets/normalize.scss';
 import './stylesheets/app.scss';
 
+import {Motion, spring} from 'react-motion';
+
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      open: false,
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({open: !this.state.open});
+  }
+
   render () {
     return (
       <div id='app'>
-        <h1>react webpack babel es2015 mocha hmre boilerplate</h1>
-        <p>by booivus</p>
+        <Motion
+          style={{x: spring(
+            this.state.open ? 400 : 0,
+            {stiffness: 160, damping: 13}
+          )}}
+        >
+          {(spring) => {
+            console.log(spring.x);
+              return (
+                <div
+                  id='box'
+                  style={{
+                    left: `${spring.x}px`,
+                    top: `${spring.x/4}px`
+                  }}
+                  onClick={this.toggle}
+                >
+                  {spring.x}
+                </div>
+              )
+            }
+          }
+        </Motion>
       </div>
     )
   }
